3322c85a4d44818cf997c512316a7259ab52b7c2
sathwikred21 <sathwik552221@gmail.com>
2025-01-25 20:34:33 +0530

    Added files

---
+++ app.py
@@ -0,0 +1,106 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the Random Forest CLassifier model
+filename = 'first-innings-score-lr-model.pkl'
+regressor = pickle.load(open(filename, 'rb'))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        pitch=request.form['pitch']
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+            
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+
+        
+        
+            
+            
+            
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+
+        if wickets>10 or wickets<0:
+            raise Exception("Invalid Input at wickets")
+        if runs < 0 :
+            raise Exception("Negative input given at runs which is not considered")
+        if runs_in_prev_5>runs or runs_in_prev_5<0:
+            raise Exception("Inavlid Input at runs in previous 5 over input")
+        if wickets_in_prev_5>wickets:
+            raise Exception("Invalid Input at wickets in previous 5 over input")
+        
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        
+        data = np.array([temp_array])
+        my_prediction = int(regressor.predict(data)[0])
+
+        if pitch == 'greenpitch':
+            my_prediction-=6
+        elif pitch == 'deadpitch':
+            my_prediction+=2
+        elif pitch == 'dustypitch':
+            my_prediction-=2
+        elif pitch == 'wetpitch':
+            my_prediction-=4
+        elif pitch == 'drypitch':
+            my_prediction+=2
+        elif pitch == 'flattrackpitch':
+            my_prediction+=8
+        else :
+            raise Exception("Something went wrong")
+              
+        return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
